4fda3dd02cb3c8b62e590c1d424bffc8da349fc4
Julie Yang <94724821+juliejyyang@users.noreply.github.com>
2025-11-22 00:22:47 -0500

    fetch the search api

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
+import { useState } from 'react';
 import './styling.css';
 
 function SearchBar() {
+  const [item, setSearchItem] = useState('') // const [state, setState] = useState(initialState)
+  const [result, setResults] = useState([])
+
+  const handleSearch=async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchedItem = e.target.value
+    setSearchItem(searchedItem)
+
+    if (searchedItem.length < 2) { // dont search if user types less than 2 char
+      setResults([]) // clear previous results
+      return // exit the function early, don't run the search
+    }
+
+    try {
+      const res = await fetch(`http://localhost:8000/api/search?query=${searchedItem}`) // res=response of the api call
+      const items = await res.json() // Converts the response body from JSON string to JavaScript object/array, await waits for parsing to complete
+      setResults(items) // Saves the items to state. Component re-renders and displays the results
+    } catch (error){
+      console.error('Search failed:', error)
+    }
+  }
+
+  
   return(
     <form>
       <input type='text' placeholder='place your order!' className='input_box'/>
